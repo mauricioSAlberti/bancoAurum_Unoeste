421fea4fb8066e774610aff07159ed4f608a213a
Maurício <93089055+itoalberti@users.noreply.github.com>
2024-04-11 09:42:23 -0300

    Update Menu.jsx

--- frontend/src/Templates/Menu.jsx
+++ frontend/src/Templates/Menu.jsx
@@ -1,25 +1,30 @@
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default function Menu(props) 
-{
-    return (
-        <Navbar expand='lg' className='bg-body-tertiary'>
-            <Container>
-                <Navbar.Brand><Link to='/'>Menu</Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <NavDropdown title='Cadastros' id='basic-nav-dropdown'>
-                            <NavDropdown.Item><Link to='/agencia'>Agências</Link></NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item><Link to='/cliente'>Clientes</Link></NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item><Link to='/produto'>Produtos</Link></NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+export default function Menu(props) {
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Brand href='/'>HOME</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <NavDropdown title='Cadastros' id='basic-nav-dropdown'>
+              <NavDropdown.Item>
+                <Link to='/agencia'>Agências</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>
+                <Link to='/cliente'>Clientes</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>
+                <Link to='/produto'>Produtos</Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
